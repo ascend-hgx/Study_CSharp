e75e33521e458184e2ea0a6cb4cc62d600e069a1
黄尚 <forevermeforeverme@outlook.com>
2021-05-09 02:19:40 +0800

    添加文件目录判断，没有的话则创建

--- Study_CSharp/复制文件链接/CreateFileShortcut.cs
+++ Study_CSharp/复制文件链接/CreateFileShortcut.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;        // 文件处理
+
 using PublicFunctionLib;
 
 namespace Study_CSharp.复制文件链接
@@ -15,13 +17,46 @@ namespace Study_CSharp.复制文件链接
         public void Test()
         {
             Read("fileList.txt");
+            string derect = GetDirectory(fileDateList[1]);
+            Console.WriteLine(derect);
+            string fileName = GetFileName(fileDateList[1]);
+            Console.WriteLine(fileName);
+            CreateDirectory("NewDir");
         }
-        public void Read(string fileName)
+        public List<string> Read(string fileName)
         {
             fileDateList = myFile.Read(fileName);
             for (int i = 0; i < fileDateList.Count(); i++)
             {
                 Console.WriteLine(fileDateList[i]);
+            }
+            return fileDateList;
+        }
+        public string GetDirectory(string file)
+        {
+            string[] listStr = file.Split('\\');
+            StringBuilder strBdr = new StringBuilder();
+            for (int i = 0; i < listStr.Length - 1; i++)
+            {
+                // Console.WriteLine(listStr[i]);
+                if(i < listStr.Length - 2)
+                    strBdr.Append(listStr[i] + '\\');
+                else
+                    strBdr.Append(listStr[i]);
+            }
+
+            return strBdr.ToString();
+        }
+        public string GetFileName(string file)
+        {
+            string[] listStr = file.Split('\\');
+            return listStr[listStr.Length - 1];
+        }
+        public void CreateDirectory(string dirct)
+        {
+            if (!Directory.Exists(dirct))
+            {
+                Directory.CreateDirectory(dirct);
             }
         }
     }
